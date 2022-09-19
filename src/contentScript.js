e9a9db7d57c94a10e3649f575bf9cda4bc977e9e
D4NZ-jpg <57308957+D4NZ-jpg@users.noreply.github.com>
2022-09-19 10:12:41 -0500

    Basic Style

--- src/contentScript.js
+++ src/contentScript.js
@@ -4,34 +4,42 @@ import './style.scss';
 function createHeader(RTitle, RAuthor, RDate) {
   // Make header
   let header = document.createElement('div');
+  header.className = 'header';
 
   // Make title
   if (RTitle) {
     let title = document.createElement('h1');
+    title.id = 'title';
     title.textContent = RTitle;
     header.appendChild(title);
   }
 
+  // Data div
+  let data = document.createElement('div');
+  data.id = 'metadata';
+
   // Make author
   if (RAuthor) {
-    let author = document.createElement('h3');
+    let author = document.createElement('h4');
+    author.id = 'author';
     author.textContent = RAuthor;
-    header.appendChild(author);
+    data.appendChild(author);
   }
 
   // Make date
   if (RDate) {
     let date = document.createElement('h4');
+    date.id = 'date';
     date.textContent = new Date(Date.parse(RDate)).toLocaleDateString('en-US', {
       weekday: 'long',
       year: 'numeric',
       month: 'long',
       day: 'numeric',
     });
-    header.appendChild(date);
+    data.appendChild(date);
   }
 
-  header.appendChild(document.createElement('br'));
+  header.appendChild(data);
   return header;
 }
 
@@ -53,13 +61,16 @@ if (document.getElementById('clarity-reader-webContainer')) {
     // Make the container for the article
     let container = document.createElement('div');
     container.innerHTML = r.content;
-    container.id = 'clarity-reader-articleContainer';
+    container.id = 'articleContainer';
 
     //Add container to document
-    document.body.appendChild(container);
     container.insertBefore(
       createHeader(r.title, r.author, r.date_published),
       container.firstChild
     );
+    let page = document.createElement('div');
+    page.id = 'clarity-reader-articleContainer';
+    page.appendChild(container);
+    document.body.appendChild(page);
   });
 }
